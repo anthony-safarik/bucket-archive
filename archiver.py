5c9c560bbeb449111aa746d35d650d84e58d5385
Anthony Safarik <anthony.safarik@gmail.com>
2025-12-25 09:13:53 -0800

    manifest and archiver tests pass

--- archiver.py
+++ archiver.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# TODO need to generalize write chunks and write data. right now this is only testing ok with groups, need to address dupes and oversize
 import os
 import csv
 import pickle
@@ -6,40 +7,43 @@ import hashlib
 from datetime import datetime
 from pathlib import Path
 
-def calculate_md5(file_path, block_size=65536):
-    """Calculate md5 checksum from file path"""
-    md5 = hashlib.md5()
-    with open(file_path, 'rb') as f:
-        for block in iter(lambda: f.read(block_size), b''):
-            md5.update(block)
-    return md5.hexdigest()
+class Manifest:
+    def __init__(self, source):
+        self.source = source
+        self.parent_directory = os.path.dirname(self.source)
+        self.output_csv = os.path.join(self.parent_directory, 'file_manifest.csv')
+        print(self.source)
 
-def get_file_info(file_path, root):
-    """returns ['File Path', 'Bytes', 'MD5', 'Timestamp']"""
-    file_size = os.path.getsize(file_path)
-    file_md5 = calculate_md5(file_path)
-    file_timestamp = os.path.getmtime(file_path)
-    timestamp_str = datetime.fromtimestamp(file_timestamp).strftime('%Y-%m-%d %H:%M:%S')
-    relative_path = os.path.relpath(file_path, root)
-    return relative_path, file_size, file_md5, timestamp_str
+    def generate_file_manifest(self):
+        
+        with open(self.output_csv, mode='w', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(['File Path', 'Bytes', 'MD5', 'Timestamp'])
 
-def write_csv(csv_file_path, list_of_rows):
-    """
-    Writes a csv file
-    
-    :param csv_file_path: sting, file path of csv file to write
-    :param list_of_rows: list containing csv.DictReader rows
-    """
+            for dirpath, dirnames, filenames in os.walk(self.source):
+                dirnames.sort()
+                for filename in sorted(filenames):
+                    if not filename.startswith('.'):
+                        file_path = os.path.join(dirpath, filename)
+                        file_info = self.get_file_info(file_path, self.source)
+                        csv_writer.writerow(file_info)
 
+    def calculate_md5(self, file_path, block_size=65536):
+        """Calculate md5 checksum from file path"""
+        md5 = hashlib.md5()
+        with open(file_path, 'rb') as f:
+            for block in iter(lambda: f.read(block_size), b''):
+                md5.update(block)
+        return md5.hexdigest()
 
-    def dict_filter(iterable_of_dicts, *keys):
-        for d in iterable_of_dicts:
-            yield dict((k, d[k]) for k in keys)
-
-    with open(csv_file_path, "w", newline='', encoding='utf-8') as f:
-        writer = csv.DictWriter(f, fieldnames=["File Path", "Bytes", "MD5", "Timestamp"])
-        writer.writeheader()
-        writer.writerows(dict_filter(list_of_rows, "File Path", "Bytes", "MD5", "Timestamp"))
+    def get_file_info(self, file_path, root):
+        """returns ['File Path', 'Bytes', 'MD5', 'Timestamp']"""
+        file_size = os.path.getsize(file_path)
+        file_md5 = self.calculate_md5(file_path)
+        file_timestamp = os.path.getmtime(file_path)
+        timestamp_str = datetime.fromtimestamp(file_timestamp).strftime('%Y-%m-%d %H:%M:%S')
+        relative_path = os.path.relpath(file_path, root)
+        return relative_path, file_size, file_md5, timestamp_str
 
 def write_data(list_of_rows, asset_path):
     """
@@ -55,25 +59,10 @@ def write_data(list_of_rows, asset_path):
         old_filepath = os.path.join(origin,filepath)
         new_filepath = os.path.join(asset_path,filepath)
         #move or copy logic goes here
-        # print(f"{old_filepath}\n-->{new_filepath}")
+        print(f"{old_filepath}\n-->{new_filepath}")
+        os.makedirs(os.path.dirname(new_filepath), exist_ok=True)
+        os.rename(old_filepath,new_filepath)
     print(f"Total GB = {round(total_bytes / 1000 ** 3,2)}")
-
-def generate_file_manifest(folder_path):
-    parent_directory = os.path.dirname(folder_path)
-    output_csv = os.path.join(parent_directory, 'file_manifest.csv')
-    
-    with open(output_csv, mode='w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(['File Path', 'Bytes', 'MD5', 'Timestamp'])
-
-        for dirpath, dirnames, filenames in os.walk(folder_path):
-            for filename in filenames:
-                if not filename.startswith('.'):
-                    file_path = os.path.join(dirpath, filename)
-                    file_info = get_file_info(file_path, folder_path)
-                    csv_writer.writerow(file_info)
-
-    print(f"File manifest created: {output_csv}")
 
 def verify_file_manifest(csv_file, expected_header = ['File Path', 'Bytes', 'MD5', 'Timestamp']):
     """
@@ -106,165 +95,139 @@ def verify_file_manifest(csv_file, expected_header = ['File Path', 'Bytes', 'MD5
                 
     return True
 
-def group_files(csv_files, chunk_size = 50 * 1000**3, avoid_duplicates = True, seen_md5 = set()):
-    """
-    Processes a csv or a list of csv files. Returns the csvs in chunks based on size.
-    Option to avoid duplicates and accept a set of known md5 to check against
-    
-    :param csv_files: list of csv files to process
-    :param chunk_size: integer, size of each chunk in bytes (default to 50GB)
-    :param avoid_duplicates: True/False, filter out duplicate files (default True)
-    :param seen_md5: set of existing md5 to mark as duplicates (duplicates within the csv_files list will be added)
-    """
-    duplicates = []
-    chunks = []
-    current_chunk = []
-    current_size = 0
-
-    for csv_file in csv_files:
-        with open(csv_file, newline='', encoding='utf-8') as f:
-            reader = csv.DictReader(f)
-            
-            for row in reader:
-                file_path = row["File Path"]
-                size = int(row["Bytes"])
-                md5 = row["MD5"]
-                timestamp = row["Timestamp"]
-                # Add a new key-value pair
-                row["Origin"] = csv_file.replace("file_manifest.csv","assets")
-
-                # Check for duplicates
-                if avoid_duplicates and md5 in seen_md5:
-                    duplicates.append(row)
-                    continue
-                seen_md5.add(md5)
-
-                # If adding this file exceeds required chunk size, start a new chunk
-                if current_size + size > chunk_size:
-                    chunks.append(current_chunk)
-                    current_chunk = []
-                    current_size = 0
-
-                current_chunk.append(row)
-                current_size += size
-
-    # Add the last chunk if not empty
-    if current_chunk:
-        chunks.append(current_chunk)
-
-    return chunks, duplicates
-
 def load_pkl(filepath):
     with open(filepath, "rb") as f:
         loaded_data = pickle.load(f)
     return loaded_data
 
-def write_chunks(chunks, output_dir, chunk_prefix = "CHK-", start_chunk = 1, mode = "move"):
-    """
-    converts a list of lists that contain dicts to chunked csvs
-    
-    :param chunks: list of lists containing file dicts for writing to csv
-    :param output_dir: string, dir to write chunked csv files to
-    :param chunk_prefix: string, prefix for the chunk buckets
-    :param start_chunk: integer, number for the first chunk
-    """
-    if mode == "csv":
-        # Make output dir
-        os.makedirs(output_dir, exist_ok=True)
-        # Write chunks to separate CSV files
-        for i, chunk in enumerate(chunks, start_chunk):
-            filename = f"{output_dir}/{chunk_prefix}{str(i).zfill(4)}.csv"
-            write_csv(filename, chunk)
-            print(f"Written {len(chunk)} files to {filename}")
-
-    if mode == "move":
-        # Make output dir
-        os.makedirs(output_dir, exist_ok=True)
-        # Write chunks to separate CSV files
-        for i, chunk in enumerate(chunks, start_chunk):
-            asset_folder_path = f"{output_dir}/{chunk_prefix}{str(i).zfill(4)}/assets"
-            os.makedirs(asset_folder_path, exist_ok=True)
-            filename = f"{output_dir}/{chunk_prefix}{str(i).zfill(4)}/file_manifest.csv"
-            write_csv(filename, chunk)
-            write_data(chunk, asset_folder_path)
-            print(f"Written {len(chunk)} files to {filename}")
-
-
-
-
-
 class Archiver:
-    def __init__(self, source, output_dir="output", bucket_size= 50 * 1000 ** 3, dedupe=False, prefix="BDL-", truth=None):
-        # self.source = Path(source)
-        self.source = source
+    def __init__(self, csv_files, output_dir="output", mode= "move", bucket_size= 50 * 1000 ** 3, start_num=1, dedupe=False, prefix="BDL-", seen_md5 = set()):
+        self.csv_files = csv_files
+        self.output_dir = Path(output_dir)
+        self.mode = mode
         self.bucket_size = bucket_size
+        self.start_num = start_num
         self.dedupe = dedupe
         self.prefix = prefix
-        self.truth = truth
-        self.output_dir = Path(output_dir)
+        self.seen_md5 = seen_md5
 
     def run(self):
-        print(f"Archiving from {self.source}")
+        print(f"Archiving from {self.csv_files}")
         print(f"Bucket size: {self.bucket_size} bytes")
         print(f"Dedupe: {self.dedupe}")
         print(f"Prefix: {self.prefix}")
-        print(f"Truth table: {self.truth}")
+        print(f"Seen MD5: {self.seen_md5}")
         print(f"Output directory: {self.output_dir}")
 
-        # Create output directory if needed
-        # self.output_dir.mkdir(parents=True, exist_ok=True)
+        self.groups, self.dupes, self.oversized = self.group_files()
+        self.write_chunks()
+        # for group in self.groups:
+        #     write_data(group,self.output_dir/"assets")
 
-        # Walk files
-        # files = list(self.source.rglob("*"))
-        # files = [f for f in files if f.is_file()]
-
-        # Placeholder: your real logic goes here
-        # current_bucket = []
-        # current_size = 0
-        # bucket_index = 1
-
-        # for file in files:
-        #     size = file.stat().st_size
-
-        #     if current_size + size > self.bucket_size:
-        #         # self._write_bucket(bucket_index, current_bucket)
-        #         bucket_index += 1
-        #         current_bucket = []
-        #         current_size = 0
-
-        #     current_bucket.append(file)
-        #     current_size += size
-
-        # if current_bucket:
-        #     print(bucket_index, current_size)
-            # self._write_bucket(bucket_index, current_bucket)
-
-    # def _write_bucket(self, index, files):
-    #     bucket_path = self.output_dir / f"bucket_{index}"
-    #     bucket_path.mkdir(exist_ok=True)
-
-    #     print(f"Writing bucket {index} with {len(files)} files")
-
-    #     for file in files:
-    #         dest = bucket_path / file.name
-    #         dest.write_bytes(file.read_bytes())
-
-if __name__ == "__main__":
-    # Import vars
-    from config import *
-
-    # Create the archiver object
-    archiver = Archiver(SOURCE, OUTPUT_DIR, BUCKET_SIZE, DEDUPE, PREFIX, TRUTH_PICKLE)
-    archiver.run()
-    # exit()
+    def write_csv(self, csv_file_path, list_of_rows):
+        """
+        Writes a csv file
+        
+        :param csv_file_path: sting, file path of csv file to write
+        :param list_of_rows: list containing csv.DictReader rows
+        """
 
 
-    # import glob
-    # csv_files = sorted(glob.glob(f"{INPUT_DIR}/*/file_manifest.csv"))
+        def dict_filter(iterable_of_dicts, *keys):
+            for d in iterable_of_dicts:
+                yield dict((k, d[k]) for k in keys)
+
+        with open(csv_file_path, "w", newline='', encoding='utf-8') as f:
+            writer = csv.DictWriter(f, fieldnames=["File Path", "Bytes", "MD5", "Timestamp"])
+            writer.writeheader()
+            writer.writerows(dict_filter(list_of_rows, "File Path", "Bytes", "MD5", "Timestamp"))
+
+    def group_files(self):
+        """
+        Processes a csv or a list of csv files. Returns the csvs in chunks based on size.
+        Option to avoid duplicates and accept a set of known md5 to check against
+        
+        :param csv_files: list of csv files to process
+        :param self.bucket_size: integer, size of each chunk in bytes (default to 50GB)
+        :param self.dedupe: True/False, filter out duplicate files (default True)
+        :param seen_md5: set of existing md5 to mark as duplicates (duplicates within the csv_files list will be added)
+        """
+        duplicates = []
+        chunks = []
+        oversized = []
+        current_chunk = []
+        current_size = 0
+
+        for csv_file in self.csv_files:
+            with open(csv_file, newline='', encoding='utf-8') as f:
+                reader = csv.DictReader(f)
+                
+                for row in reader:
+                    file_path = row["File Path"]
+                    size = int(row["Bytes"])
+                    md5 = row["MD5"]
+                    timestamp = row["Timestamp"]
+                    # Add a new key-value pair
+                    row["Origin"] = csv_file.replace("file_manifest.csv","assets")
+
+                    # Check for oversized
+                    if size > self.bucket_size:
+                        oversized.append(row)
+                        continue
+
+                    # Check for duplicates
+                    if self.dedupe and md5 in self.seen_md5:
+                        duplicates.append(row)
+                        continue
+                    self.seen_md5.add(md5)
+
+                    # If adding this file exceeds required chunk size, start a new chunk
+                    if current_size + size > self.bucket_size:
+                        chunks.append(current_chunk)
+                        current_chunk = []
+                        current_size = 0
+
+                    current_chunk.append(row)
+                    current_size += size
+
+        # Add the last chunk if not empty
+        if current_chunk:
+            chunks.append(current_chunk)
+
+        return chunks, duplicates, oversized
+
+    def write_chunks(self):
+        # Make output dir
+        os.makedirs(self.output_dir, exist_ok=True)
+        # Write chunks to separate CSV files
+        for i, chunk in enumerate(self.groups, self.start_num):
+            asset_folder_path = f"{self.output_dir}/{self.prefix}{str(i).zfill(4)}/assets"
+            os.makedirs(asset_folder_path, exist_ok=True)
+            filename = f"{self.output_dir}/{self.prefix}{str(i).zfill(4)}/file_manifest.csv"
+            self.write_csv(filename, chunk)
+            if self.mode == "move":
+                write_data(chunk, asset_folder_path)
+                print(f"-----------------Written {len(chunk)} files to {filename}")
 
 
-    truth_table = load_pkl(TRUTH_PICKLE)
-    groups, dupes = group_files(SOURCE, BUCKET_SIZE, DEDUPE, truth_table)
 
-    write_chunks(groups, OUTPUT_DIR, PREFIX, start_chunk=1)
-    write_chunks([dupes], OUTPUT_DIR, chunk_prefix = "DUP-", start_chunk = 1, mode = "csv")
+
+# if __name__ == "__main__":
+#     # Import vars
+#     from config import *
+
+#     # Create the archiver object
+#     archiver = Archiver(SOURCE, OUTPUT_DIR, BUCKET_SIZE, DEDUPE, PREFIX, TRUTH_PICKLE)
+#     archiver.run()
+#     # exit()
+
+
+#     # import glob
+#     # csv_files = sorted(glob.glob(f"{INPUT_DIR}/*/file_manifest.csv"))
+
+
+#     truth_table = load_pkl(TRUTH_PICKLE)
+#     groups, dupes = group_files(SOURCE, BUCKET_SIZE, DEDUPE, truth_table)
+
+#     write_chunks(groups, OUTPUT_DIR, PREFIX, start_chunk=1)
+#     write_chunks([dupes], OUTPUT_DIR, chunk_prefix = "DUP-", start_chunk = 1, mode = "csv")
--- test_archiver.py
+++ test_archiver.py
@@ -2,8 +2,7 @@ import unittest
 import os
 import shutil
 from archiver import Archiver
-from archiver import calculate_md5
-from manifest import Manifest
+from archiver import Manifest
 
 class TestArchiver(unittest.TestCase):
     """Basic test cases."""
@@ -18,10 +17,24 @@ class TestArchiver(unittest.TestCase):
         self.test_disc_size = 50 #bytes
         self.test_full_disc_size = 45 #bytes
 
+        # Define directories in a dictionary
+        self.directories = {
+            "Landing": os.path.join(self.test_root, "01_Landing"),
+            "ToChunk": os.path.join(self.test_root, "02_ToChunk"),
+            "Chunking": os.path.join(self.test_root, "03_Chunking"),
+            "Staging": os.path.join(self.test_root, "04_Staging"),
+            "Archive": os.path.join(self.test_root, "05_Archive"),
+        }
+
+        # Create directories if they don't exist
+        for path in self.directories.values():
+            os.makedirs(path, exist_ok=True)
+
+
         # Create some subfolders and files for testing
         self.subfolders = [("TestFiles_10bytes", 10), ("TestFiles_15bytes", 15), ("TestFiles_55bytes", 55)]
         for folder in self.subfolders:
-            folder_path = os.path.join(self.test_root, "To_Chunk", "TestFiles", "assets", folder[0])
+            folder_path = os.path.join(self.test_root, "02_ToChunk", "TestFiles", "assets", folder[0])
             os.makedirs(folder_path, exist_ok=True)
             for i in range(5):
 
@@ -37,13 +50,27 @@ class TestArchiver(unittest.TestCase):
         return super().tearDown()
     
     def test_Manifest(self):
-        self.manifest = Manifest(os.path.join(self.test_root, "To_Chunk", "TestFiles", "assets"))
-        self.manifest.generate_file_manifest()
+        pass
 
+    def test_Archiver(self):
+        print("---Testing Manifest---")
+        self.manifest = Manifest(os.path.join(self.test_root, "02_ToChunk", "TestFiles", "assets"))
+        self.manifest.generate_file_manifest()
+        print("---Testing Archiver---")
+        # self.archiver = Archiver([os.path.join(self.test_root, "02_ToChunk", "TestFiles", "assets", "file_manifest.csv")])
+        self.archiver = Archiver([self.manifest.output_csv], output_dir= self.directories["Chunking"], bucket_size= 50)
+        self.archiver.run()
+        # for group in self.archiver.groups:
+        #     for f in group:
+        #         print (f["File Path"])
+        #     print ("___")
         null = input("PAUSED")
 
+
     def test_calculate_md5(self):
-        trashme_md5 = calculate_md5('trashme.log')
+        print("---Testing MD5---")
+        manifest = Manifest("No Path")
+        trashme_md5 = manifest.calculate_md5('trashme.log')
         assert(trashme_md5 == '93b885adfe0da089cdf634904fd59f71')
 
 if __name__ == '__main__':
