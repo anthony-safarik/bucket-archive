cfbe3186c7684d809ff72e15579923720d57f0fc
Anthony Safarik <anthony.safarik@gmail.com>
2026-04-26 07:28:33 -0700

    added chunker test

--- test_bucket_archive.py
+++ test_bucket_archive.py
@@ -2,8 +2,10 @@ import unittest
 import os
 import shutil
 import time
+import csv
 
 from manifest import Manifest
+from chunker import Chunker
 
 class TestBasic(unittest.TestCase):
     """Basic test cases."""
@@ -12,23 +14,6 @@ class TestBasic(unittest.TestCase):
 
         self.test_root = "trash_testing"
         self.init_time = time.strftime("%y%m%d%H%M%S")
-        self.test_load = f"{self.test_root}/test-load-{self.init_time}"
-        self.test_load_fail = f"{self.test_root}/test-load-{self.init_time}_fail"
-
-        def make_some_files(load_name, sizes, number_of_files =5):
-            for size in sizes:
-                subname= f"TestFiles_{str(size).zfill(2)}bytes"
-                folder_path = os.path.join(load_name, "assets", subname)
-                os.makedirs(folder_path, exist_ok=True)
-                for i in range(number_of_files):
-                    test_file_name = f'{subname}_{i}.txt'
-                    with open(f'{folder_path}/{test_file_name}', 'wb') as f:
-                        f.write(b'\0' * size)  # Write null bytes
-
-        file_sizes = list(range(1, 10))
-        file_sizes_fail = list(range(1, 9))
-        make_some_files(self.test_load,file_sizes,1)
-        make_some_files(self.test_load_fail,file_sizes_fail,1)
         return super().setUp()
     
     def tearDown(self):
@@ -37,7 +22,16 @@ class TestBasic(unittest.TestCase):
 
     def test_manifest(self):
         print("---Testing Manifest---")
-        manifest = Manifest(f"{self.test_load}/assets")
+
+        test_manifest_load = f"{self.test_root}/manifest/loads/test-load-{self.init_time}"
+        test_manifest_load_fail = f"{self.test_root}/manifest/loads/test-load-{self.init_time}_fail"
+
+        file_sizes = list(range(1, 10))
+        file_sizes_fail = list(range(1, 9))
+        self.make_some_files(test_manifest_load,file_sizes,1)
+        self.make_some_files(test_manifest_load_fail,file_sizes_fail,1)
+
+        manifest = Manifest(f"{test_manifest_load}/assets")
         manifest_file = manifest.generate_file_manifest()
 
         assert os.path.isfile(manifest_file) == True
@@ -48,7 +42,7 @@ class TestBasic(unittest.TestCase):
 
 
         # File missing case
-        manifest_file_fail = os.path.join(self.test_load_fail,'file_manifest.csv')
+        manifest_file_fail = os.path.join(test_manifest_load_fail,'file_manifest.csv')
         shutil.copy2(manifest_file,manifest_file_fail)
         assert os.path.isfile(manifest_file_fail) == True
         verify_manifest = Manifest(manifest_file_fail)
@@ -56,7 +50,7 @@ class TestBasic(unittest.TestCase):
         assert result == False
 
         #File mismatch case
-        failed_file_dir = f'{self.test_load_fail}/assets/TestFiles_09bytes'
+        failed_file_dir = f'{test_manifest_load_fail}/assets/TestFiles_09bytes'
         failed_file = f'{failed_file_dir}/TestFiles_09bytes_0.txt' #the missing file
         os.makedirs (failed_file_dir)
 
@@ -66,7 +60,51 @@ class TestBasic(unittest.TestCase):
         result = verify_manifest.verify_file_manifest(manifest_file_fail, expected_header = True)
         assert result == False
 
+    def test_chunker(self):
+        print("---Testing Chunker---")
+        chunker_load_parent = f"{self.test_root}/chunker/loads"
+        chunker_chunks_dir = f"{self.test_root}/chunker/chunks"
+        chunker_load_a = f"{self.test_root}/chunker/loads/test-files_{self.init_time}"
+
+
+        file_sizes = list(range(1, 9))
+        self.make_some_files(chunker_load_a,file_sizes,1)
+        manifest = Manifest(f"{chunker_load_a}/assets")
+        manifest_file = manifest.generate_file_manifest()
+        bytes_to_chunk = 12
+        bytes_to_gb = bytes_to_chunk / (1000**3)
+        this_chunker = Chunker(chunker_load_parent,chunker_chunks_dir,bytes_to_gb,False)
+        this_chunker.run()
+
+        csv_files = os.listdir(chunker_chunks_dir)
+
+        for csv_file in csv_files:
+            if csv_file.startswith("chunk_"):
+                csv_file = os.path.join(chunker_chunks_dir,csv_file)
+                this_csv_total_bytes = 0
+                with open(csv_file, newline='', encoding='utf-8') as f:
+                    csv_reader = csv.DictReader(f)
+
+                    for row in csv_reader:
+                        file_bytes = int(row["Bytes"])
+                        this_csv_total_bytes += file_bytes
+                # print(csv_file,this_csv_total_bytes)
+                assert this_csv_total_bytes <= bytes_to_chunk
+
+
+
         null = input("PAUSED")
+
+    @staticmethod
+    def make_some_files(load_name, sizes, number_of_files =5):
+        for size in sizes:
+            subname= f"TestFiles_{str(size).zfill(2)}bytes"
+            folder_path = os.path.join(load_name, "assets", subname)
+            os.makedirs(folder_path, exist_ok=True)
+            for i in range(number_of_files):
+                test_file_name = f'{subname}_{i}.txt'
+                with open(f'{folder_path}/{test_file_name}', 'wb') as f:
+                    f.write(b'\0' * size)  # Write null bytes
 
 if __name__ == '__main__':
     unittest.main()
