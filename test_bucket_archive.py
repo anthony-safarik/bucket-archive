5588ed0416d45b479dbf14fc7e41ce6a09334ae2
Tony Safarik <anthony.safarik@gmail.com>
2026-04-30 07:19:59 -0700

    chunker test added

--- test_bucket_archive.py
+++ test_bucket_archive.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import time
 import csv
+import mover
 
 from manifest import Manifest
 from chunker import Chunker
@@ -64,15 +65,19 @@ class TestBasic(unittest.TestCase):
         print("---Testing Chunker---")
         chunker_load_parent = f"{self.test_root}/chunker/loads"
         chunker_chunks_dir = f"{self.test_root}/chunker/chunks"
-        chunker_load_a = f"{self.test_root}/chunker/loads/test-files_{self.init_time}"
-
-
-        file_sizes = list(range(1, 16))
-        self.make_some_files(chunker_load_a,file_sizes,1)
-        manifest = Manifest(f"{chunker_load_a}/assets")
-        manifest_file = manifest.generate_file_manifest()
+        chunker_load_c = f"{self.test_root}/chunker/loads/test-files_a_{self.init_time}"
+        chunker_load_b = f"{self.test_root}/chunker/loads/test-files_b_{self.init_time}"
         bytes_to_chunk = 12
         bytes_to_gb = bytes_to_chunk / (1000**3)
+
+        # Make some files in two different loads
+        file_sizes = list(range(1, 16))
+        self.make_some_files(chunker_load_c,file_sizes,1)
+        self.make_some_files(chunker_load_b,file_sizes,1)
+        manifest_a = Manifest(f"{chunker_load_c}/assets")
+        manifest_file_a = manifest_a.generate_file_manifest()
+        manifest_b = Manifest(f"{chunker_load_b}/assets")
+        manifest_file_b = manifest_b.generate_file_manifest()
         this_chunker = Chunker(chunker_load_parent,chunker_chunks_dir,bytes_to_gb,False)
         this_chunker.run()
 
@@ -100,20 +105,27 @@ class TestBasic(unittest.TestCase):
 
                 # Check that the csv is not oversized
                 assert this_csv_total_bytes <= bytes_to_chunk
-                print (f'this_csv_total_bytes {this_csv_total_bytes} <= bytes_to_chunk {bytes_to_chunk}')
+                # print (f'this_csv_total_bytes {this_csv_total_bytes} <= bytes_to_chunk {bytes_to_chunk}')
 
                 csv_counter += 1
 
                 # Check that the first file of the next chunk can not fit inside the previous
                 if csv_counter > 1:
                     assert prev_csv_total_bytes + first_file_bytes > bytes_to_chunk
-                    print(f'prev_csv_total_bytes {prev_csv_total_bytes} + first_file_bytes {first_file_bytes} > bytes_to_chunk {bytes_to_chunk}\n')
+                    # print(f'prev_csv_total_bytes {prev_csv_total_bytes} + first_file_bytes {first_file_bytes} > bytes_to_chunk {bytes_to_chunk}\n')
                 prev_csv_total_bytes = this_csv_total_bytes
 
+            # Check for handling of oversized files, dupes and path collisions (right now it should just exit)
+            elif csv_file.startswith("oversized_"):
+                csv_file = os.path.join(chunker_chunks_dir,csv_file)
+                with open(csv_file, newline='', encoding='utf-8') as f:
+                    csv_reader = csv.DictReader(f)
+                    for row in csv_reader:
+                        file_bytes = int(row["Bytes"])
+                        file_path = row["File Path"]
+                        assert bytes_to_chunk < file_bytes
+                        # print(f'{file_path.split('/')[-1]} file_bytes {file_bytes} > bytes_to_chunk {bytes_to_chunk}')
 
-
-
-        null = input("PAUSED")
 
     @staticmethod
     def make_some_files(load_name, sizes, number_of_files =5):
@@ -125,6 +137,20 @@ class TestBasic(unittest.TestCase):
                 test_file_name = f'{subname}_{i}.txt'
                 with open(f'{folder_path}/{test_file_name}', 'wb') as f:
                     f.write(b'\0' * size)  # Write null bytes
+
+    @staticmethod
+    def get_summary(input_path):
+        total_size = 0
+        file_count = 0
+        if os.path.isdir(input_path):
+            for root, dirs, files in os.walk(input_path):
+                dirs.sort()
+                for file_name in sorted(files):
+                    if not file_name.startswith("._"):
+                        total_size += os.path.getsize(os.path.join(root, file_name))
+                        file_count += 1
+        return (file_count, total_size)
+
 
 if __name__ == '__main__':
     unittest.main()
